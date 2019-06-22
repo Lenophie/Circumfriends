eff5b6c35200bfd8238472113be184c7a19a6596
Lenophie <quasiphies@gmail.com>
2019-06-22 12:55:59 +0200

    Add friend-related controllers

---
+++ Assets/Scripts/Controllers/FriendAttractionController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Controllers {
+    public class FriendAttractionController {
+        private readonly Rigidbody2D friendRigidbody;
+        private readonly Rigidbody2D meRigidbody;
+        private float attractionModifier;
+
+        public FriendAttractionController(Rigidbody2D friendRigidbody, Rigidbody2D meRigidbody) {
+            this.friendRigidbody = friendRigidbody;
+            this.meRigidbody = meRigidbody;
+            attractionModifier = 0.5f;
+        }
+
+        public void AttractMe() {
+            meRigidbody.AddForce((friendRigidbody.position - meRigidbody.position) * attractionModifier);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/FriendController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    public class FriendController : MonoBehaviour {
+        [SerializeField] private Rigidbody2D friendRigidbody = default;
+        [SerializeField] private Rigidbody2D meRigidbody = default;
+        private FriendAttractionController friendAttractionController;
+
+        private void Start() {
+            friendAttractionController = new FriendAttractionController(friendRigidbody, meRigidbody);
+        }
+
+        private void Update() {
+            friendAttractionController.AttractMe();
+        }
+    }
+}
