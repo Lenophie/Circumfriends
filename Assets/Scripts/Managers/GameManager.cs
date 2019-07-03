52b02f9b49ff0128f1c5327ff4d739cbf45de478
Lenophie <quasiphies@gmail.com>
2019-07-03 11:47:57 +0200

    Improve transition test method

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -28,6 +28,12 @@ namespace Managers {
             yield return new WaitForSeconds(5f);
             friendZonesController.FriendZones.Comfort.FriendZoneShape.TransitionToNewCharacteristics(new FriendZoneShapeConfig(
                 6f, 0.1f, 3f));
+            yield return new WaitForSeconds(1f);
+            friendZonesController.FriendZones.Discomfort.FriendZoneShape.TransitionToNewCharacteristics(new FriendZoneShapeConfig(
+                3f, 0.05f, 8f));
+            yield return new WaitForSeconds(2f);
+            friendZonesController.FriendZones.Comfort.FriendZoneShape.TransitionToNewCharacteristics(new FriendZoneShapeConfig(
+                4f, 0.05f, 20f));
         }
 
         private void Update() {
