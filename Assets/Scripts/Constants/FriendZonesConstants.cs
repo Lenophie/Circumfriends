fe94264b36080cc28490d063d5f0f00e2fe741bb
Lenophie <quasiphies@gmail.com>
2019-07-03 12:34:43 +0200

    Add FriendZones' colors to FriendZonesConstants

--- Assets/Scripts/Constants/FriendZonesConstants.cs
+++ Assets/Scripts/Constants/FriendZonesConstants.cs
@@ -1,11 +1,30 @@
+using UnityEngine;
+
 namespace Constants {
     public static class FriendZonesConstants {
         public static int NumberOfOuterVerticesPerFriendzone { get; private set; }
         public static float MaxLineNoiseAmplitude { get; private set; }
 
+        public static Color NoGoZoneOut { get; private set; }
+        public static Color NoGoZoneIn { get; private set; }
+        public static Color DiscomfortZoneOut { get; private set; }
+        public static Color DiscomfortZoneIn { get; private set; }
+        public static Color ComfortZoneOut { get; private set; }
+        public static Color ComfortZoneIn { get; private set; }
+        public static Color DistantZoneOut { get; private set; }
+        public static Color DistantZoneIn { get; private set; }
+
         public static void SetConstants(FriendZonesConstantsCollector friendZonesConstantsCollector) {
             NumberOfOuterVerticesPerFriendzone = friendZonesConstantsCollector.numberOfOuterVerticesPerFriendzone;
             MaxLineNoiseAmplitude = friendZonesConstantsCollector.maxLineNoiseAmplitude;
+            NoGoZoneOut = friendZonesConstantsCollector.noGoZoneOut;
+            NoGoZoneIn = friendZonesConstantsCollector.noGoZoneIn;
+            DiscomfortZoneOut = friendZonesConstantsCollector.discomfortZoneOut;
+            DiscomfortZoneIn = friendZonesConstantsCollector.discomfortZoneIn;
+            ComfortZoneOut = friendZonesConstantsCollector.comfortZoneOut;
+            ComfortZoneIn = friendZonesConstantsCollector.comfortZoneIn;
+            DistantZoneOut = friendZonesConstantsCollector.distantZoneOut;
+            DistantZoneIn = friendZonesConstantsCollector.distantZoneIn;
         }
     }
 }
--- Assets/Scripts/Constants/FriendZonesConstantsCollector.cs
+++ Assets/Scripts/Constants/FriendZonesConstantsCollector.cs
@@ -1,9 +1,21 @@
 using System;
+using UnityEngine;
 
 namespace Constants {
     [Serializable]
     public class FriendZonesConstantsCollector {
+        [Header("Parameters")]
         public int numberOfOuterVerticesPerFriendzone;
         public float maxLineNoiseAmplitude;
+
+        [Header("Colors")]
+        public Color noGoZoneOut;
+        public Color noGoZoneIn;
+        public Color discomfortZoneOut;
+        public Color discomfortZoneIn;
+        public Color comfortZoneOut;
+        public Color comfortZoneIn;
+        public Color distantZoneOut;
+        public Color distantZoneIn;
     }
 }
