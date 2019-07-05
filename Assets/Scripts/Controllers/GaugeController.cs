ddcda5a6cc36c3e39a46169557f524923a2ab190
Lenophie <quasiphies@gmail.com>
2019-07-06 01:18:01 +0200

    Add GaugeController

---
+++ Assets/Scripts/Controllers/GaugeController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    [ExecuteAlways]
+    public class GaugeController : MonoBehaviour {
+        [SerializeField] private RectTransform gaugeTransform;
+        [SerializeField] private RectTransform gaugeFillTransform;
+        [Range(0, 1)] public float gaugeFill;
+
+        private void Update() {
+            gaugeFillTransform.sizeDelta = new Vector2(gaugeFillTransform.sizeDelta.x, -gaugeTransform.rect.height * gaugeFill);
+        }
+    }
+}
