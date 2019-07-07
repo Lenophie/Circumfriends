f4aba131e4be307dda49f03cca09a706447f0edf
Lenophie <quasiphies@gmail.com>
2019-07-07 16:30:56 +0200

    Add Start button to menu scene

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Managers {
+    public class MenuManager : MonoBehaviour {
+        public void HandleStartPress() {
+            Debug.Log("pressed");
+        }
+    }
+}
