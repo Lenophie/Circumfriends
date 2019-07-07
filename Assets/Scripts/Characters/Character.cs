0cd6ad6fc590ced6eca228bac34539dae913843d
Lenophie <quasiphies@gmail.com>
2019-07-08 01:13:36 +0200

    Fix Character using UnityEditor namespace (build error)

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,10 +1,9 @@
-using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Characters {
     [CreateAssetMenu(fileName = "New Character", menuName = "Character")]
     public class Character : ScriptableObject {
         public new string name;
-        public AnimatorController animatorController;
+        public RuntimeAnimatorController animatorController;
     }
 }
