4f46b55fef8c130f6231a28e11c089d0eecb9e14
Lenophie <quasiphies@gmail.com>
2019-07-15 17:28:55 +0200

    Add Muje sprite and animation

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -6,8 +6,7 @@ namespace Managers {
     public static class ScenesManager {
         public static IEnumerator LoadScene(string sceneName) {
             AsyncOperation asyncLoading = SceneManager.LoadSceneAsync(sceneName);
-
             while (!asyncLoading.isDone) yield return null;
-            }
+        }
     }
 }
